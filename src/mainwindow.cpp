f9648a0ca54bc1ce7071b7dff0c508bf3e0cfdeb
lib64 <michael.harper2@g.austincc.edu>
2019-12-02 16:51:07 -0600

    worked on main window

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,20 +1,67 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QPainter>
 #include <iostream>
 
-#include "lexer.h"
-#include "parser.h"
-#include "interpreter.h"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    _scene = new QGraphicsScene(this);
+    ui->graphicsView->setScene(_scene);
+
+
+    /*
+    QPainter painter(&image);
+    QPen pen(Qt::white);
+    pen.setWidth(5);
+    painter.setPen(pen);
+    painter.drawLine(0,0,100,100);
+    */
+
+    QImage image(640,480,QImage::Format::Format_RGB32);
+    image.fill(Qt::white);
+    _pixmapItem = new QGraphicsPixmapItem(QPixmap::fromImage(image));
+    _scene->addItem(_pixmapItem);
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::graphFunction(double (Interpreter::*function)(double), Interpreter &interpreter)
+{
+    std::cout << (interpreter.*function)(10.0) << std::endl;
+}
+
+void MainWindow::on_lineEdit_textChanged(const QString &arg1)
+{
+    try {
+        Lexer lexer(arg1.toStdString());
+        vector<LexerToken> tokens = lexer.lex();
+
+        Parser parser(tokens);
+        AstNode *root = parser.parseMain();
+
+        Interpreter interpreter(root);
+        graphFunction(&Interpreter::interpret, interpreter);
+
+    } catch (...) {
+
+    }
+}
+
+
+
+
+
+
+
+
+
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -1,7 +1,13 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include "lexer.h"
+#include "parser.h"
+#include "interpreter.h"
+
 #include <QMainWindow>
+#include <QGraphicsScene>
+#include <QGraphicsPixmapItem>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +21,16 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+    void graphFunction(double (Interpreter::*func)(double), Interpreter &interpreter);
+
+private slots:
+    void on_lineEdit_textChanged(const QString &arg1);
+
+
 private:
     Ui::MainWindow *ui;
+    QGraphicsScene *_scene;
+    QGraphicsPixmapItem *_pixmapItem;
 };
 
 #endif // MAINWINDOW_H
